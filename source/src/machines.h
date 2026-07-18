744604dc6cbb46251e02338bce54f36439e0b466
Mamigu2022 <157837973+Mamigu2022@users.noreply.github.com>
2026-07-18 23:29:26 +0200

    Add files via upload

--- source/src/machines.h
+++ source/src/machines.h
@@ -4,30 +4,33 @@
 // disable e.g. if roms are missing
 #define ENABLE_PACMAN
 #define ENABLE_GALAGA
-#define ENABLE_DKONG
+//#define ENABLE_DKONG
 //#define ENABLE_FROGGER
 //#define ENABLE_DIGDUG
 #define ENABLE_1942
-#define ENABLE_EYES
-#define ENABLE_MRTNT
-#define ENABLE_LIZWIZ
-#define ENABLE_THEGLOB
-#define ENABLE_CRUSH
-#define ENABLE_ANTEATER
+//#define ENABLE_EYES
+//#define ENABLE_MRTNT
+//#define ENABLE_LIZWIZ
+//#define ENABLE_THEGLOB
+//#define ENABLE_CRUSH
+//#define ENABLE_ANTEATER
 #define ENABLE_BOMBJACK
 //#define ENABLE_MRDO
 #define ENABLE_BAGMAN
 //#define ENABLE_PENGO
-#define ENABLE_GYRUSS
+//#define ENABLE_GYRUSS
+#define ENABLE_STARFORCE
+#define ENABLE_MOONCRESTA
 #define ENABLE_MSPACMAN
 #define ENABLE_GALAXIAN
 #define ENABLE_SPACE
-// #define ENABLE_TIMEPLT //Little buggy so commented out.
+// #define ENABLE_TIMEPLT
 //#define ENABLE_TUTANKHM
 //#define ENABLE_LADYBUG
 //#define ENABLE_DKONGJR
-
-
+#define ENABLE_MOONCRESTA
+//#define ENABLE_SCRAMBLE
+//#define ENABLE_SUPERCOBRA
 #ifdef ENABLE_PACMAN  
   #include "machines/pacman/pacman.h"
 #endif
@@ -96,27 +99,7 @@
   #include "machines/gyruss/gyruss.h"
 #endif
 
-#ifdef ENABLE_MSPACMAN
-  #include "machines/mspacman/mspacman.h"
-#endif
-
-#ifdef ENABLE_GALAXIAN
-  #include "machines/galaxian/galaxian.h"
-#endif
-
-#ifdef ENABLE_SPACE
-  #include "machines/spaceinvaders/spaceinvaders.h"
-#endif
-
-#ifdef ENABLE_TIMEPLT
-  #include "machines/timeplt/timeplt.h"
-#endif
-
-#ifdef ENABLE_TUTANKHM
-  #include "machines/tutankhm/tutankhm.h"
-#endif
-
-#ifdef ENABLE_LADYBUG
+#ifdef ENABLE_LADYBUG  
   #include "machines/ladybug/ladybug.h"
 #endif
 
@@ -124,84 +107,146 @@
   #include "machines/dkongjr/dkongjr.h"
 #endif
 
-// change machine order is possible here...
-machineBase *machines[] = {
-#ifdef ENABLE_PACMAN  
-  new pacman(),
+#ifdef ENABLE_MSPACMAN  
+  #include "machines/mspacman/mspacman.h"
 #endif
-#ifdef ENABLE_GALAGA  
-  new galaga(), 
-#endif  
-#ifdef ENABLE_DIGDUG  
-  new digdug(), 
-#endif  
-#ifdef ENABLE_FROGGER  
-  new frogger(), 
-#endif  
-#ifdef ENABLE_DKONG  
-  new dkong(), 
-#endif  
-#ifdef ENABLE_1942  
-  new _1942(), 
-#endif  
-#ifdef ENABLE_LIZWIZ  
-  new lizwiz(),
-#endif  
-#ifdef ENABLE_EYES  
-  new eyes(), 
-#endif  
-#ifdef ENABLE_MRTNT  
-  new mrtnt(), 
-#endif  
-#ifdef ENABLE_THEGLOB 
-  new theglob(),
+
+#ifdef ENABLE_TIMEPLT 
+  #include "machines/timeplt/timeplt.h"
 #endif
-#ifdef ENABLE_CRUSH 
-  new crush(),
+
+#ifdef ENABLE_TUTANKHM
+  #include "machines/tutankhm/tutankhm.h"
 #endif
-#ifdef ENABLE_ANTEATER 
-  new anteater(),
+
+#ifdef ENABLE_SPACE 
+  #include "machines/spaceinvaders/spaceinvaders.h"
 #endif
-#ifdef ENABLE_BOMBJACK 
-  new bombjack(),
+
+#ifdef ENABLE_GALAXIAN 
+  #include "machines/galaxian/galaxian.h"
 #endif
-#ifdef ENABLE_MRDO 
-  new mrdo(),
+
+#ifdef ENABLE_STARFORCE
+  #include "machines/starforce/starforce.h"
 #endif
-#ifdef ENABLE_BAGMAN 
-  new bagman(),
+
+#ifdef ENABLE_MOONCRESTA
+  #include "machines/mooncresta/mooncresta.h"
 #endif
-#ifdef ENABLE_PENGO 
-  new pengo(),
+
+#ifdef ENABLE_SCRAMBLE
+  #include "machines/scramble/scramble.h"
 #endif
-#ifdef ENABLE_GYRUSS 
-  new gyruss(),
+
+#ifdef ENABLE_SUPERCOBRA
+  #include "machines/supercobra/supercobra.h"
+#endif
+
+#ifdef ENABLE_DKONG3
+  #include "machines/dkong3/dkong3.h"
+#endif
+
+#ifdef ENABLE_SUPERCOBRA
+  #include "machines/supercobra/supercobra.h"
+#endif
+// ── Array de punteros — se rellena en initMachines() ─────────
+// NO se usan 'new' aquí para evitar inicialización estática
+// antes de que el heap de Bluepad32 esté listo.
+// Tamaño máximo = número de juegos compilados
+#define MAX_MACHINES 32
+extern machineBase *machines[MAX_MACHINES];
+extern signed char machinesCount;
+
+// Llama a esta función al principio de setup(), antes de cualquier
+// otra inicialización, para crear los objetos de juego en el heap.
+inline void initMachines() {
+  machinesCount = 0;
+#ifdef ENABLE_PACMAN
+  machines[machinesCount++] = new pacman();
+#endif
+#ifdef ENABLE_GALAGA
+  machines[machinesCount++] = new galaga();
+#endif
+#ifdef ENABLE_DIGDUG
+  machines[machinesCount++] = new digdug();
+#endif
+#ifdef ENABLE_FROGGER
+  machines[machinesCount++] = new frogger();
+#endif
+#ifdef ENABLE_DKONG
+  machines[machinesCount++] = new dkong();
+#endif
+#ifdef ENABLE_1942
+  machines[machinesCount++] = new _1942();
+#endif
+#ifdef ENABLE_LIZWIZ
+  machines[machinesCount++] = new lizwiz();
+#endif
+#ifdef ENABLE_EYES
+  machines[machinesCount++] = new eyes();
+#endif
+#ifdef ENABLE_MRTNT
+  machines[machinesCount++] = new mrtnt();
+#endif
+#ifdef ENABLE_THEGLOB
+  machines[machinesCount++] = new theglob();
+#endif
+#ifdef ENABLE_CRUSH
+  machines[machinesCount++] = new crush();
+#endif
+#ifdef ENABLE_ANTEATER
+  machines[machinesCount++] = new anteater();
+#endif
+#ifdef ENABLE_BOMBJACK
+  machines[machinesCount++] = new bombjack();
+#endif
+#ifdef ENABLE_MRDO
+  machines[machinesCount++] = new mrdo();
+#endif
+#ifdef ENABLE_BAGMAN
+  machines[machinesCount++] = new bagman();
+#endif
+#ifdef ENABLE_PENGO
+  machines[machinesCount++] = new pengo();
+#endif
+#ifdef ENABLE_GYRUSS
+  machines[machinesCount++] = new gyruss();
 #endif
 #ifdef ENABLE_MSPACMAN
-  new mspacman(),
+  machines[machinesCount++] = new mspacman();
 #endif
 #ifdef ENABLE_GALAXIAN
-  new galaxian(),
+  machines[machinesCount++] = new galaxian();
 #endif
 #ifdef ENABLE_SPACE
-  new spaceinvaders(),
+  machines[machinesCount++] = new spaceinvaders();
 #endif
 #ifdef ENABLE_TIMEPLT
-  new timeplt(),
+  machines[machinesCount++] = new timeplt();
 #endif
 #ifdef ENABLE_TUTANKHM
-  new tutankhm(),
+  machines[machinesCount++] = new tutankhm();
 #endif
-#ifdef ENABLE_DKONGJR  
-  new dkongjr(),
+#ifdef ENABLE_DKONGJR
+  machines[machinesCount++] = new dkongjr();
 #endif
 #ifdef ENABLE_LADYBUG
-  new ladybug()
+  machines[machinesCount++] = new ladybug();
 #endif
-};
-
-template <std::size_t N, class T>
-constexpr std::size_t countof(T(&)[N]) { return N; }
-static_assert(countof(machines) >= 1, "At least one machine has to be enabled!");
+#ifdef ENABLE_STARFORCE
+  machines[machinesCount++] = new starforce();
+#endif
+#ifdef ENABLE_MOONCRESTA
+  machines[machinesCount++] = new mooncresta();
+#endif
+#ifdef ENABLE_SCRAMBLE
+  machines[machinesCount++] = new scramble();
+#endif
+#ifdef ENABLE_SUPERCOBRA
+  machines[machinesCount++] = new supercobra();
+#endif
+  static_assert(MAX_MACHINES >= 32, "MAX_MACHINES demasiado pequeño");
+}
 
 #endif // _MACHINES_H_
